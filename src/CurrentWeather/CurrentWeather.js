f0b4bd1854a5af50cc635828db4384c991aad3ff
Andrew Kinstler <andrewckinstler@gmail.com>
2020-01-29 13:10:47 -0700

    Style CurrentWeather component

--- src/CurrentWeather/CurrentWeather.js
+++ src/CurrentWeather/CurrentWeather.js
@@ -1,19 +1,25 @@
 import React from 'react';
 import './CurrentWeather.scss';
+import moment from 'moment';
 
 export const CurrentWeather = props => {
   const { name, weather, main, dt } = props
   const date = new Date(dt * 1000);
-  console.log(date)
+
   return (
-    <div>
-      <h2>{name}</h2>
-      <p>{date.toString()}</p>
-      <p>{weather[0].main}</p>
-      <p>{weather[0].description}</p>
-      <p>Current temp: {main.temp}°</p>
-      <p>High: {main.temp_max}°</p>
-      <p>Low: {main.temp_min}°</p>
-    </div>
+    <section>
+      <div className='today'>
+        <div className='current-container'>
+          <h2>{name}</h2>
+          <p className='date'>{moment(date).calendar()}</p>
+          <p className='current-temp'>{main.temp}°</p>
+          <p>{weather[0].main}</p>
+        </div>
+        <div className='minmax'>
+          <p><span className='bold'>H:</span> {main.temp_max}° / <span className='bold'>L:</span> {main.temp_min}°</p>
+          <p>{weather[0].description}</p>
+        </div>
+      </div>
+    </section>
   )
 };
